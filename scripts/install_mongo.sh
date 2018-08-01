47092399c93006c464b793c105e763fda8f1bfb6
Italo Batista <italo.batista@ccc.ufcg.edu.br>
2018-08-01 14:10:41 -0300

    Add script to install mongodb in vm

---
+++ scripts/install_mongo.sh
@@ -0,0 +1,21 @@
+#!/BIN/BASH
+
+# Add Mong Repo
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+
+# Install and verify
+echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+
+sudo apt-get update
+
+sudo apt-get install -y mongodb-org
+
+sudo mv mongodb.service /etc/systemd/system/
+
+# Start services
+
+sudo systemctl start mongodb
+
+sudo systemctl status mongodb
+
+sudo systemctl enable mongodb
