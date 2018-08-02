475381cb93cd2a0ce7cb024d48e4bdb71b72c811
Italo Batista <italo.batista@ccc.ufcg.edu.br>
2018-08-02 07:55:01 -0300

    Add script to populate db

---
+++ scripts/populate_db.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+from pymongo import MongoClient
+import json
+from pprint import pprint
+import sys
+import os
+
+client = MongoClient('localhost', 27017)
+db = client.recsys
+
+HOME_USER = os.path.expanduser('~')
+path = HOME_USER +  "/data/recsys/"
+
+playlists_collection = db.playlists
+playlists_collection.delete_many({})
+
+for filename in os.listdir(path):
+	file_path = os.path.join(path, filename)
+
+	with open(file_path) as f:
+		data = json.load(f)
+
+		playlists_jsons = data['playlists']
+
+		for playlist in playlists_jsons:
+			playlists_collection.insert_one(playlist)
